6ee68ef922777219eaf22620c75796edf53e0f90
Manser <tj218@live.missouristate.edu>
2018-04-09 11:48:48 -0500

    added the trashMan

--- ReportingModule.py
+++ ReportingModule.py
@@ -3,19 +3,32 @@ import json
 class reportingModule:
     #test tuple to create JSON object
     #box num, class, num, accuracy
-    def __init__(self, tuples):
-        self.results = []
-        for frame in tuples:
+    def __init__(self, tuples): 
+        self.results = {}
+        for i in range(len(tuples)): #puts the data dictionary (temp) into the dictionary (results)
             temp = {}
-            temp["num_Bugs"] = frame[0]
-            temp["accuracy"] = frame[1]
-            temp["class"] = frame[2]
-            self.results.append(temp)
+            temp["num_Bugs"] = tuples[i][0]
+            temp["accuracy"] = tuples[i][1]
+            temp["class"] = tuples[i][2]
+            self.results[i] = temp
         
  
-
+    def convert2Json(self): # changes key of overall dictionary to string
+        return (json.dumps(self.results))
+    
+    def trashMan(self): # deletes any tuples(Frames) that are irrelevent   
+        for i in range(len(self.results)):
+            # checks accuracy of bug and deletes any tuples that have less 60% accuracy 
+            if self.results[i]["accuracy"] < 60: 
+                del self.results[i]
+            # checks to see if any bugs were detected on screen 
+            elif self.results[i]["num_Bugs"] < 1:
+                del self.results[i]
+            
 def main():
-    test = [(4, 92, 3),(5,85,3),(6,82,3)] 
-    print(reportingModule(test).results) 
+    test = [(4, 92, 3),(5,50,3),(6,82,3),(0,0,0)]
+    report = reportingModule(test)
+    report.trashMan()
+    print(report.convert2Json())
 
 main()
